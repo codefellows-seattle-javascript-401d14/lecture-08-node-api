330d9c42b0797f625b772eda7b33e32751bff239
devonhackley <devonhackley@gmail.com>
2017-01-12 12:15:44 -0800

    committing last nights lab

--- server.js
+++ server.js
@@ -2,13 +2,17 @@
 
 const http = require('http');
 const PORT = process.env.PORT || 3000;
-const server = http.createServer((req,res) => {
-  res.statusCode = 404;
-  res.write('route Not Found');
-  res.end();
-  return;
+const Router = require('./lib/router.js');
+const pokemonRouter = require('./router/pokemon-router.js');
 
-});
+let router = new Router();
+
+pokemonRouter(router);
+
+
+//returns function with req,res
+const server = http.createServer(router.route());
+
 
 server.listen(PORT, () => {
   console.log('server is up! OVOXO');
