501038ce58ce965567eb965ee023a305f652b176
tommytnez <tommytnez@yahoo.com>
2017-01-16 18:16:27 -0800

    added fs and bluebird

--- test/comment-router-test.js
+++ test/comment-router-test.js
@@ -29,55 +29,52 @@ describe('testing /api/comments', function(){
       });
     });
 
-    describe('with invalid input', function(){
-    });
-  });
+    describe('testing GET', function(){
+      describe('with valid input', function(){
+        // mock a Comment so that we have an id to make a get to
+        before((done) => {
+          this.tempComment = new Comment({announcer: 'Troy Aikman', comment: 'The team that scores touchdowns is gonna win more often than not'});
+          storage.setItem('comments', this.tempComment)
+          .then(() => done())
+          .catch(done);
+        });
 
-  describe('testing GET', function(){
-    describe('with valid input', function(){
-      // mock a Comment so that we have an id to make a get to
-      before((done) => {
-        this.tempComment = new Comment({announcer: 'Troy Aikman', comment: 'The team that scores touchdowns is gonna win more often than not'});
-        storage.setItem('comments', this.tempComment)
-        .then(() => done())
-        .catch(done);
+        it('should return a comment', (done) => {
+          superagent.get(`${apiURL}/api/comments?id=${this.tempComment.id}`)
+          .then(res => {
+            expect(res.status).to.equal(200);
+            expect(res.body.announcer).to.equal(this.tempComment.announcer);
+            expect(res.body.comment).to.equal(this.tempComment.comment);
+            expect(res.body.id).to.equal(this.tempComment.id);
+            expect(new Date(res.body.created).toString()).to.equal(this.tempComment.created.toString());
+            expect(Boolean(res.body.created)).to.equal(true);
+            expect(Boolean(res.body.id)).to.equal(true);
+            done();
+          })
+          .catch(done);
+        });
       });
 
-      it('should return a comment', (done) => {
-        superagent.get(`${apiURL}/api/comments?id=${this.tempComment.id}`)
-        .then(res => {
-          expect(res.status).to.equal(200);
-          expect(res.body.announcer).to.equal(this.tempComment.announcer);
-          expect(res.body.comment).to.equal(this.tempComment.comment);
-          expect(res.body.id).to.equal(this.tempComment.id);
-          expect(new Date(res.body.created).toString()).to.equal(this.tempComment.created.toString());
-          expect(Boolean(res.body.created)).to.equal(true);
-          expect(Boolean(res.body.id)).to.equal(true);
-          done();
-        })
-        .catch(done);
-      });
-    });
+      describe('with invalid input', function(){
+        it('get /api/comments with no id should return a 400 status', (done) => {
+          superagent.get(`${apiURL}/api/comments`)
+          .then(done)
+          .catch(err => {
+            expect(err.status).to.equal(400);
+            done();
+          })
+          .catch(done);
+        });
 
-    describe('with invalid input', function(){
-      it('get /api/comments with no id should return a 400 status', (done) => {
-        superagent.get(`${apiURL}/api/comments`)
-        .then(done)
-        .catch(err => {
-          expect(err.status).to.equal(400);
-          done();
-        })
-        .catch(done);
-      });
-
-      it('get /api/comments with bad id should return a 404 status', (done) => {
-        superagent.get(`${apiURL}/api/comments?id=54321`)
-       .then(done)
-       .catch(err => {
-         expect(err.status).to.equal(404);
-         done();
-       })
-       .catch(done);
+        it('get /api/comments with bad id should return a 404 status', (done) => {
+          superagent.get(`${apiURL}/api/comments?id=54321`)
+          .then(done)
+          .catch(err => {
+            expect(err.status).to.equal(404);
+            done();
+          })
+          .catch(done);
+        });
       });
     });
   });
